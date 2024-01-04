1a61ada8a394254348893926eda75ed466d716fb
Sergey Yakovlev <3gnees@gmail.com>
2024-01-04 22:32:26 +0300

    Added public documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 //! Library for building distributed systems in Rust
 //! with support for testing and debugging in simulation.
 
-// Add warnings for missing public and private documentation.
+// Add warnings for missing public documentation.
 #![warn(missing_docs)]
-#![warn(clippy::missing_docs_in_private_items)]
+
+// Add warnings for missing in private documentation (disabled for now).
+// #![warn(clippy::missing_docs_in_private_items)]
 
 pub mod common;
 
