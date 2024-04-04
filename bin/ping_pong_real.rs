5f05399f0d9012884d95d5de953455c01dccff52
Sergey Yakovlev <3gnees@gmail.com>
2024-04-05 01:03:44 +0300

    Removed outdated examples

--- bin/ping_pong_real.rs
+++
@@ -1,8 +0,0 @@
-//! Run ping-pong real ecosystem.
-
-use dsbuild::examples::ping_pong::real;
-
-fn main() {
-    const NEED_CYCLES: u32 = 100;
-    real::run_real(NEED_CYCLES);
-}
--- bin/ping_pong_sim.rs
+++
@@ -1,8 +0,0 @@
-//! Run ping-pong ecosystem simulation.
-
-use dsbuild::examples::ping_pong::sim;
-
-fn main() {
-    const NEED_CYCLES: u32 = 100;
-    sim::run_sim(NEED_CYCLES);
-}
