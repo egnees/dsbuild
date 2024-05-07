fb10c9d84b5bc98384497cc8ea7804fcad7757a3
Sergey Yakovlev <3gnees@gmail.com>
2024-05-08 00:49:55 +0300

    Added --experimental_allow_proto3_optional option.

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("src/real/proto/message_passing.proto")?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(
+            &["src/real/proto/message_passing.proto"],
+            &["src/real/proto/"],
+        )?;
     Ok(())
 }
