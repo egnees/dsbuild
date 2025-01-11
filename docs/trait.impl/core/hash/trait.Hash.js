a6c610313f1f4232c95dd1343bc87e19428a6d6b
egnees <egnees@users.noreply.github.com>
2025-01-11 11:10:55 +0000

    deploy: 228580d2bdd3f156c52f365216e6f63524dc714e

--- docs/trait.impl/core/hash/trait.Hash.js
+++ docs/trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["dsbuild",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"dsbuild/struct.Address.html\" title=\"struct dsbuild::Address\">Address</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"dsbuild/struct.Message.html\" title=\"struct dsbuild::Message\">Message</a>"]]]]);
+    var implementors = Object.fromEntries([["dsbuild",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"dsbuild/struct.Address.html\" title=\"struct dsbuild::Address\">Address</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"dsbuild/struct.Message.html\" title=\"struct dsbuild::Message\">Message</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- docs/trait.impl/core/ops/deref/trait.Deref.js
+++ docs/trait.impl/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["dsbuild",[["impl&lt;P: <a class=\"trait\" href=\"dsbuild/trait.Process.html\" title=\"trait dsbuild::Process\">Process</a> + 'static&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"dsbuild/struct.ProcessGuard.html\" title=\"struct dsbuild::ProcessGuard\">ProcessGuard</a>&lt;'_, P&gt;"]]]]);
+    var implementors = Object.fromEntries([["dsbuild",[["impl&lt;P: <a class=\"trait\" href=\"dsbuild/trait.Process.html\" title=\"trait dsbuild::Process\">Process</a> + 'static&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"dsbuild/struct.ProcessGuard.html\" title=\"struct dsbuild::ProcessGuard\">ProcessGuard</a>&lt;'_, P&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
